ff5a50c2927049c4c358aa1ab391728d7b8f5de0
yunusemreozdemir <emreozdemir@mail.com>
2024-05-09 06:06:07 +0300

    add navbar, tabs, add, delete match session and create squad ui

--- frontend/src/pages/Coach.tsx
+++ frontend/src/pages/Coach.tsx
@@ -1,8 +1,9 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Navigate} from 'react-router-dom'
 import { useAuth } from '../hooks'
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-
-import { Button } from "@/components/ui/button"
 
 export default function Coach () {
     const navigate = useNavigate()
@@ -13,20 +14,134 @@ export default function Coach () {
 
     const user = getAuth()
 
-    console.log(user)
+    const [activeTab, setActiveTab] = React.useState('addMatchSession')
+
+    const [addTabState, setAddTabState] = React.useState({stadiumName: "", stadiumCountry: "", date: "", timeSlot: "", juryName: "", jurySurname: ""})
+    const [deleteTabState, setDeleteTabState] = React.useState('')
+    const [createTabState, setCreateTabState] = React.useState({ usernames: [], inputValue: ""})
+    const [viewTabState, setViewTabState] = React.useState([])
+ 
 
     return (
-        <div>
-            <h1>Coach Page</h1>
-            <Button onClick={() => navigate('/')}>Go to Home</Button><br /><br />
-            <Button onClick={() => navigate('/DeleteMatchSession')}>Delete match session</Button><br /><br />
-            <Button onClick={() => navigate('/AddMatchSession')}>Add match session</Button><br /><br />
-            <Button onClick={() => navigate('/CreateSquad')}>Create squad</Button><br /><br />
-            <Button onClick={() => navigate('/ViewStadiums')}>View stadiums</Button><br /><br />
-            <Button onClick={() => {
-                logout()
-                navigate('/')
-            }}>Logout</Button><br /><br />
+        <div className='h-screen w-screen flex flex-col'>
+            <div className='flex-initial flex flex-row justify-between bg-zinc-900 text-white py-3 px-5'>
+                <h1 className='text-xl'>Coach</h1>
+                <div className='flex flex-row bg-zinc-300 p-1 text-base gap-1 rounded-sm text-zinc-800'>
+                    <button className={activeTab === "addMatchSession" ? "bg-white rounded-sm px-4" : "px-4"} onClick={() => setActiveTab('addMatchSession')}>Add Match Session</button>
+                    <button className={activeTab === "deleteMatchSession" ? "bg-white rounded-sm px-4" : "px-4"} onClick={() => setActiveTab('deleteMatchSession')}>Delete Match Session</button>
+                    <button className={activeTab === "createSquad" ? "bg-white rounded-sm px-4" : "px-4"} onClick={() => setActiveTab('createSquad')}>Create Squad</button>
+                    <button className={activeTab === "viewStadiums" ? "bg-white rounded-sm px-4" : "px-4"} onClick={() => {
+                        setActiveTab('viewStadiums'); 
+                        axios.get(`http://localhost:8000/api/get-stadiums/`)
+                            .then(function (response) {
+                                console.log(response.data.stadiums);
+                                setViewTabState(response.data.stadiums);
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+                    }}>View Stadiums</button>
+                </div>
+                <button onClick={() => {logout(); navigate('/')}}><LogoutOutlinedIcon className='text-2xl'/></button>
+            </div>
+            <div className='h-full w-full flex-auto flex justify-center'>
+                <div className='flex flex-col justify-center'>
+                    <div className='shadow-sm rounded-md border p-7'>
+                        {
+                            activeTab === 'addMatchSession' && (
+                                <div>
+                                    <input placeholder='Stadium Name' className='border' value={addTabState.stadiumName} onChange={(e) => setAddTabState((prev) => {return { ...prev, stadiumName: e.target.value}})}/>
+                                    <input placeholder='Stadium Country' className='border' value={addTabState.stadiumCountry} onChange={(e) => setAddTabState((prev) => {return { ...prev, stadiumCountry: e.target.value}})}/>
+                                    <input placeholder='Date' className='border' value={addTabState.date} onChange={(e) => setAddTabState((prev) => {return { ...prev, date: e.target.value}})}/>
+                                    <input placeholder='Time Slot' className='border' value={addTabState.timeSlot} onChange={(e) => setAddTabState((prev) => {return { ...prev, timeSlot: e.target.value}})}/>
+                                    <input placeholder='Jury Name' className='border' value={addTabState.juryName} onChange={(e) => setAddTabState((prev) => {return { ...prev, juryName: e.target.value}})}/>
+                                    <input placeholder='Jury Surname' className='border' value={addTabState.jurySurname} onChange={(e) => setAddTabState((prev) => {return { ...prev, jurySurname: e.target.value}})}/>
+                                    <button onClick={() => {
+                                        axios.post(`http://localhost:8000/api/add-match-session/`, {
+                                            "coach_username": user.user[0],
+                                            "stadium_name": addTabState.stadiumName,
+                                            "stadium_country": addTabState.stadiumCountry,
+                                            "date": addTabState.date,
+                                            "time_slot": addTabState.timeSlot,
+                                            "jury_name": addTabState.juryName,
+                                            "jury_surname": addTabState.jurySurname
+                                        
+                                        })
+                                        .then(function (response) {
+                                            setAddTabState({stadiumName: "", stadiumCountry: "", date: "", timeSlot: "", juryName: "", jurySurname: ""});
+                                        })
+                                        .catch(function (error) {
+                                          console.log(error);
+                                        }); 
+                                    }}>Add</button>
+                                </div>
+                            )
+                        }
+                        {
+                            activeTab === 'deleteMatchSession' && (
+                                <div>
+                                    <input placeholder='Session ID' className='' value={deleteTabState} onChange={(e) => setDeleteTabState(e.target.value)}/>
+                                    <button onClick={() => {
+                                        axios.post(`http://localhost:8000/api/delete-match-session/`, {"session_ID": deleteTabState})
+                                        .then(function (response) {
+                                            setDeleteTabState('');
+                                        })
+                                        .catch(function (error) {
+                                          console.log(error);
+                                        }); 
+                                    }}>Delete</button>
+                                </div>
+                            )
+                        }
+                        {
+                            activeTab === 'createSquad' && (
+                                <div>
+                                    <div className="flex flex-col">
+                                    <input className="border" placeholder='Session ID'/>
+                                    <input className="border" placeholder="Enter play usernames" value={createTabState.inputValue} onKeyDown={
+                                        (event: React.KeyboardEvent<HTMLInputElement>) => {
+                                            if (event.key === 'Enter') {
+                                                setCreateTabState((prev) => {return { usernames: [...prev.usernames, prev.inputValue], inputValue: ''}})
+                                            }
+                                        }
+                                    } onChange={(e) => setCreateTabState((prev) => {return { ...prev, inputValue: e.target.value}})} />
+                                    </div>
+                                    <ul>
+                                        {createTabState.usernames.map((username) => (
+                                            <li key={username}>{username}</li>
+                                        ))}
+                                    </ul>
+                                    <button onClick={() => {
+                                        axios.post(`http://localhost:8000/api/create-squad/`, {"session_ID": deleteTabState})
+                                        .then(function (response) {
+                                            setDeleteTabState({ usernames: [], inputValue: ""});
+                                        })
+                                        .catch(function (error) {
+                                          console.log(error);
+                                        }); 
+                                    }}>Create</button>
+                                </div>
+                            )
+                        }
+                        {
+                            activeTab === 'viewStadiums' && (
+                                <div>
+                                    <ul>
+                                        {viewTabState.map((stadium) => (
+                                            <li key={stadium[0]}>
+                                                <div className='flex flex-row justify-between gap-5'>
+                                                    <p>{stadium[0]}</p>
+                                                    <p>{stadium[1]}</p>
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
